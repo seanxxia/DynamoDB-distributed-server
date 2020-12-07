607f8545950d1eca183ced45005834a76491ab67
Jevan Chan <jevan.cnchan@gmail.com>
2020-12-06 19:58:20 -0800

    Add always fail test

--- src/mydynamotest/e2e_test.go
+++ src/mydynamotest/e2e_test.go
@@ -2,8 +2,11 @@ package mydynamotest
 
 import (
 	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
 )
 
 var _ = Describe("End-to-End", func() {
-	// keep
+	FIt("should fail.", func() {
+		Expect(1 + 1).To(Equal(3))
+	})
 })
