74d75f6c51b6cc0c87938472ba3e7ee4105fee37
Sean <zixia@ucsd.edu>
2020-12-08 15:34:05 -0800

    Add test cases

--- src/mydynamotest/released_test.go
+++ src/mydynamotest/released_test.go
@@ -7,16 +7,11 @@ import (
 )
 
 var _ = Describe("TA Released Tests", func() {
-	// Reference:
-	// - https://piazza.com/class/kfqynl4r6a0317?cid=1074
-	// - https://github.com/ucsd-cse-x24-fa20/module4/blob/main/src/mydynamotest/released_test.go#L76
 
-	Describe("Two Servers", func() {
-
+	Describe("twoserver.ini: clusterSize:5 r-Value:3 w-Value:3", func() {
 		var sc ServerCoordinator
 
 		BeforeEach(func() {
-			// StartingPort: 8000, R-Value: 2, W-Value: 2, ClusterSize: 5
 			sc = NewServerCoordinator(8000+config.GinkgoConfig.ParallelNode*100, 2, 2, 5)
 		})
 
@@ -25,6 +20,9 @@ var _ = Describe("TA Released Tests", func() {
 		})
 
 		It("should pass test: PutW2", func() {
+			// Reference:
+			// - https://github.com/ucsd-cse-x24-fa20/module4/blob/main/src/mydynamotest/released_test.go#L9
+
 			sc.GetClient(0).Put(MakePutFreshEntry("s1", []byte("abcde")))
 
 			res1 := sc.GetClient(1).Get("s1")
@@ -34,14 +32,46 @@ var _ = Describe("TA Released Tests", func() {
 			}))
 		})
 
+		It("should pass test: GossipW2", func() {
+			// Reference:
+			// - https://github.com/ucsd-cse-x24-fa20/module4/blob/main/src/mydynamotest/released_test.go#L219
+
+			sc.GetClient(0).Put(MakePutFreshEntry("s1", []byte("abcde")))
+
+			sc.GetClient(0).Gossip()
+
+			res1 := sc.GetClient(1).Get("s1")
+			Expect(res1).NotTo(BeNil())
+			Expect(GetEntryValues(res1)).To(ConsistOf([][]byte{
+				[]byte("abcde"),
+			}))
+
+			entry := res1.EntryList[0]
+			entry.Value = []byte("efghi")
+
+			sc.GetClient(1).Put(MakePutFromEntry("s1", entry))
+
+			res1 = sc.GetClient(1).Get("s1")
+			Expect(res1).NotTo(BeNil())
+			Expect(GetEntryValues(res1)).To(ConsistOf([][]byte{
+				[]byte("efghi"),
+			}))
+
+			res0 := sc.GetClient(0).Get("s1")
+			Expect(res0).NotTo(BeNil())
+			Expect(GetEntryValues(res1)).To(ConsistOf([][]byte{
+				[]byte("efghi"),
+			}))
+
+		})
+
 	})
 
-	Describe("Five Servers", func() {
+	Describe("myconfig.ini: clusterSize:5 r-Value:1 w-Value:1", func() {
 
 		var sc ServerCoordinator
 
 		BeforeEach(func() {
-			// StartingPort: 8000, R-Value: 1, W-Value: 1, ClusterSize: 5
 			sc = NewServerCoordinator(8000+config.GinkgoConfig.ParallelNode*100, 1, 1, 5)
 		})
 
@@ -50,6 +80,9 @@ var _ = Describe("TA Released Tests", func() {
 		})
 
 		It("should pass test: Gossip", func() {
+			// Reference:
+			// - https://github.com/ucsd-cse-x24-fa20/module4/blob/main/src/mydynamotest/released_test.go#L35
+
 			sc.GetClient(0).Put(MakePutFreshEntry("s1", []byte("abcde")))
 			sc.GetClient(0).Gossip()
 
@@ -62,6 +95,9 @@ var _ = Describe("TA Released Tests", func() {
 		})
 
 		It("should pass test: MultipleKeys", func() {
+			// Reference:
+			// - https://github.com/ucsd-cse-x24-fa20/module4/blob/main/src/mydynamotest/released_test.go#L62
+
 			sc.GetClient(0).Put(MakePutFreshEntry("s1", []byte("abcde")))
 
 			sc.GetClient(0).Gossip()
@@ -92,6 +128,9 @@ var _ = Describe("TA Released Tests", func() {
 		})
 
 		It("should pass test: DynamoPaper", func() {
+			// Reference:
+			// - https://github.com/ucsd-cse-x24-fa20/module4/blob/main/src/mydynamotest/released_test.go#L107
+
 			sc.GetClient(0).Put(MakePutFreshEntry("s1", []byte("abcde")))
 
 			res0 := sc.GetClient(0).Get("s1")
@@ -151,17 +190,84 @@ var _ = Describe("TA Released Tests", func() {
 
 		})
 
-		It("should pass test: TestInvalidPut", func() {
-			// Spec: If the context that the Node has already stored associated with the
-			// specified key is causally descended from the context provided to Put, i.e
-			// newContext < oldContext, Put will fail and the existing value will remain.
+		It("should pass test: InvalidPut", func() {
+			// Reference:
+			// - https://github.com/ucsd-cse-x24-fa20/module4/blob/main/src/mydynamotest/released_test.go#L199
+
 			sc.GetClient(0).Put(MakePutFreshEntry("s1", []byte("abcde")))
 			sc.GetClient(0).Put(MakePutFreshEntry("s1", []byte("efghi")))
 
-			res := sc.GetClient(0).Get("s1")
-			Expect(res).NotTo(BeNil())
-			Expect(GetEntryValues(res)).To(ConsistOf([][]byte{
+			sc.GetClient(0).Gossip()
+
+			res1 := sc.GetClient(1).Get("s1")
+			Expect(res1).NotTo(BeNil())
+			Expect(GetEntryValues(res1)).To(ConsistOf([][]byte{
 				[]byte("abcde"),
+			}))
+
+		})
+
+		It("should pass test: ReplaceMultipleVersions", func() {
+			// Reference:
+			// - https://github.com/ucsd-cse-x24-fa20/module4/blob/main/src/mydynamotest/released_test.go#L270
+
+			sc.GetClient(0).Put(MakePutFreshEntry("s1", []byte("abcde")))
+			sc.GetClient(1).Put(MakePutFreshEntry("s1", []byte("efghi")))
+
+			sc.GetClient(0).Gossip()
+
+			res1 := sc.GetClient(1).Get("s1")
+
+			entry := res1.EntryList[0]
+			entry.Context.Clock.Combine(GetEntryContextClocks(res1))
+			entry.Value = []byte("zyxw")
+			sc.GetClient(1).Put(MakePutFromEntry("s1", entry))
+
+			res1 = sc.GetClient(1).Get("s1")
+			Expect(res1).NotTo(BeNil())
+			Expect(GetEntryValues(res1)).To(ConsistOf([][]byte{
+				[]byte("zyxw"),
+			}))
+
+		})
+
+	})
+
+	Describe("consistent.ini: clusterSize:5 r-Value:3 w-Value:3", func() {
+		var sc ServerCoordinator
+
+		BeforeEach(func() {
+			sc = NewServerCoordinator(8000+config.GinkgoConfig.ParallelNode*100, 3, 3, 5)
+		})
+
+		AfterEach(func() {
+			sc.Kill()
+		})
+
+		It("should pass test: Consistent", func() {
+			// Reference:
+			// - https://github.com/ucsd-cse-x24-fa20/module4/blob/main/src/mydynamotest/released_test.go#L319
+
+			sc.GetClient(0).Put(MakePutFreshEntry("s1", []byte("abcde")))
+
+			res1 := sc.GetClient(1).Get("s1")
+			Expect(res1).NotTo(BeNil())
+			Expect(GetEntryValues(res1)).To(ConsistOf([][]byte{
+				[]byte("abcde"),
+			}))
+
+			entry := res1.EntryList[0]
+			entry.Value = []byte("zyxw")
+			sc.GetClient(3).Put(MakePutFromEntry("s1", entry))
+
+			sc.GetClient(0).Crash(3)
+			sc.GetClient(1).Crash(3)
+			sc.GetClient(4).Crash(3)
+
+			res2 := sc.GetClient(2).Get("s1")
+			Expect(res2).NotTo(BeNil())
+			Expect(GetEntryValues(res2)).To(ConsistOf([][]byte{
+				[]byte("zyxw"),
 			}))
 		})
 
