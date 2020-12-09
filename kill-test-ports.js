7b9d5295c89d4032a2407100937d59e23963b716
Jevan Chan <jevan.cnchan@gmail.com>
2020-12-08 23:11:23 -0800

    Update kill port script

--- kill-test-ports.js
+++ kill-test-ports.js
@@ -2,7 +2,7 @@ const fkill = require('fkill');
 
 (async () => {
   const ports = [];
-  for (let port = 8000; port < 9000; port++) {
+  for (let port = 8000; port < 8500; port++) {
     ports.push(`:${port}`);
   }
   await fkill(ports, { force: true, silent: true });
