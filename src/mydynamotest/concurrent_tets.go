d73cc2da8a5cfcea75857fb276f5d8f6afedb87a
Sean <zixia@ucsd.edu>
2020-12-10 22:34:34 -0800

    Fix wait

--- src/mydynamotest/concurrent_tets.go
+++ src/mydynamotest/concurrent_tets.go
@@ -1,14 +1,109 @@
 package mydynamotest
 
 import (
+	"fmt"
+	dy "mydynamo"
+	"sync"
+
 	. "github.com/onsi/ginkgo"
 	"github.com/onsi/ginkgo/config"
 	. "github.com/onsi/gomega"
 )
 
 var _ = Describe("Concurrent", func() {
+
+	Describe("Single Server", func() {
+		const randomBytesNum int = 1024
+		const concurrentClientsNum int = 100
+
+		var sc ServerCoordinator
+		BeforeEach(func() {
+			// StartingPort: 8000, R-Value: 1, W-Value: 1, ClusterSize: 1
+			sc = NewServerCoordinator(8000+config.GinkgoConfig.ParallelNode*100, 1, 1, 1)
+		})
+
+		AfterEach(func() {
+			sc.Kill()
+		})
+
+		It("should handle concurrent entries put with different keys.", func(done Done) {
+			var wg sync.WaitGroup
+
+			wg.Add(concurrentClientsNum)
+			for i := 0; i < concurrentClientsNum; i++ {
+				go func(i int) {
+					defer GinkgoRecover()
+
+					client := sc.MakeNewClient(0)
+					defer client.CleanConn()
+
+					key := fmt.Sprintf("k%d", i)
+					value := MakeRandomBytes(randomBytesNum)
+					client.Put(MakePutFreshEntry(key, value))
+
+					res := client.Get(key)
+
+					Expect(res).NotTo(BeNil())
+					Expect(GetEntryValues(res)).To(ConsistOf([][]byte{value}))
+
+					wg.Done()
+				}(i)
+			}
+
+			wg.Wait()
+			close(done)
+		}, 20.0)
+
+		It("should handle concurrent entries put with the same key.", func(done Done) {
+			var wg sync.WaitGroup
+
+			wg.Add(concurrentClientsNum)
+			for i := 0; i < concurrentClientsNum; i++ {
+				go func(i int) {
+					defer GinkgoRecover()
+
+					RandomSleep()
+
+					client := sc.MakeNewClient(0)
+					defer client.CleanConn()
+
+					res := client.Get("key")
+
+					var putArgs dy.PutArgs
+					if len(res.EntryList) == 0 {
+						putArgs = MakePutFreshEntry("key", MakeRandomBytes(randomBytesNum))
+					} else {
+						entry := res.EntryList[0]
+						entry.Value = MakeRandomBytes(randomBytesNum)
+						putArgs = MakePutFromEntry("key", entry)
+					}
+
+					client.Put(putArgs)
+					wg.Done()
+				}(i)
+			}
+			wg.Wait()
+
+			// Ignore all the previously put entries, get and put a new entry here
+			value := MakeRandomBytes(randomBytesNum)
+
+			res := sc.GetClient(0).Get("key")
+			entry := res.EntryList[0]
+			entry.Value = value
+			sc.GetClient(0).Put(MakePutFromEntry("key", entry))
+
+			res = sc.GetClient(0).Get("key")
+			Expect(res).NotTo(BeNil())
+			Expect(GetEntryValues(res)).To(ConsistOf([][]byte{value}))
+
+			close(done)
+		}, 20.0)
+	})
+
 	Describe("Multiple Servers", func() {
 		const serverNum int = 10
+		const randomBytesNum int = 1024
+		// const concurrentClientsNum int = 100
 
 		var sc ServerCoordinator
 		BeforeEach(func() {
@@ -19,18 +114,25 @@ var _ = Describe("Concurrent", func() {
 		AfterEach(func() {
 			sc.Kill()
 		})
-		It("should handle concurrent put and gossip (2)", func(done Done) {
+		FIt("should handle concurrent put and gossip (2)", func(done Done) {
+			var wg sync.WaitGroup
+
+			wg.Add(2)
 
 			sc.GetClient(5).Put(MakePutFreshEntry("k1", []byte("v1")))
 			go func(serverID int) {
 				defer GinkgoRecover()
 				sc.GetClient(serverID).Put(MakePutFreshEntry("k1", []byte("v0")))
+				wg.Done()
 			}(0)
 
 			go func(serverID int) {
 				defer GinkgoRecover()
 				sc.GetClient(serverID).Gossip()
+				wg.Done()
 			}(1)
+
+			wg.Wait()
 
 			res0 := sc.GetClient(0).Get("k1")
 			Expect(res0).NotTo(BeNil())
@@ -49,4 +151,104 @@ var _ = Describe("Concurrent", func() {
 		}, 20.0)
 	})
 
+	Describe("Multiple Servers: Exhausting", func() {
+		const serverNum int = 5
+		const randomBytesNum int = 512
+		const concurrentClientsNum int = 20
+
+		var sc ServerCoordinator
+		BeforeEach(func() {
+			// StartingPort: 8000, R-Value: 5, W-Value: 5, ClusterSize: 10
+			sc = NewServerCoordinator(8000+config.GinkgoConfig.ParallelNode*100, 2, 2, serverNum)
+		})
+
+		AfterEach(func() {
+			sc.Kill()
+		})
+
+		It("should not deadlock and handle concurrent get, put, and gossip.", func(done Done) {
+			const operationNum = 10
+
+			var wg sync.WaitGroup
+			wg.Add(concurrentClientsNum * serverNum * 3)
+
+			// Getters
+			for clientID := 0; clientID < concurrentClientsNum; clientID++ {
+				for serverID := 0; serverID < serverNum; serverID++ {
+					go func(clientID int, serverID int) {
+						defer GinkgoRecover()
+
+						client := sc.MakeNewClient(serverID)
+						defer client.CleanConn()
+
+						for opi := 0; opi < operationNum; opi++ {
+							RandomSleep()
+
+							// Do get
+							key := fmt.Sprintf("key-%d", clientID)
+							res := client.Get(key)
+							Expect(res).NotTo(BeNil())
+						}
+						wg.Done()
+					}(clientID, serverID)
+				}
+			}
+
+			// Putters
+			for clientID := 0; clientID < concurrentClientsNum; clientID++ {
+				for serverID := 0; serverID < serverNum; serverID++ {
+					go func(clientID int, serverID int) {
+						defer GinkgoRecover()
+
+						client := sc.MakeNewClient(serverID)
+						defer client.CleanConn()
+
+						for opi := 0; opi < operationNum; opi++ {
+							RandomSleep()
+
+							// Do get and put
+							key := fmt.Sprintf("key-%d", clientID)
+
+							res := client.Get(key)
+							var putArgs dy.PutArgs
+							if len(res.EntryList) == 0 {
+								putArgs = MakePutFreshEntry(key, MakeRandomBytes(randomBytesNum))
+							} else {
+								entry := res.EntryList[0]
+								entry.Context.Clock.Combine(GetEntryContextClocks(res))
+								entry.Value = MakeRandomBytes(randomBytesNum)
+								putArgs = MakePutFromEntry(key, entry)
+							}
+
+							client.Put(putArgs)
+						}
+						wg.Done()
+					}(clientID, serverID)
+				}
+			}
+
+			// Gossiper
+			for clientID := 0; clientID < concurrentClientsNum; clientID++ {
+				for serverID := 0; serverID < serverNum; serverID++ {
+					go func(clientID int, serverID int) {
+						defer GinkgoRecover()
+
+						client := sc.MakeNewClient(serverID)
+						defer client.CleanConn()
+
+						for opi := 0; opi < operationNum; opi++ {
+							RandomSleep()
+
+							// Do gossip
+							client.Gossip()
+						}
+						wg.Done()
+					}(clientID, serverID)
+				}
+			}
+
+			wg.Wait()
+			close(done)
+		}, 60.0)
+	})
 })
